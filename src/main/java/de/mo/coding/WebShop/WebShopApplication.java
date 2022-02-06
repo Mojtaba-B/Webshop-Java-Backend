fdae5c9e59d97ddb2c77942b49869577c065557f
mojtaba <bakhtiar.mojtaba@gmail.com>
2022-02-06 08:57:32 +0100

    setup project

---
+++ src/main/java/de/mo/coding/WebShop/WebShopApplication.java
@@ -0,0 +1,13 @@
+package de.mo.coding.WebShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
