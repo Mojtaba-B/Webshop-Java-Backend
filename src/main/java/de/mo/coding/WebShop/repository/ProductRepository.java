8f3a63fed65fff345e8be24b8e67d7909d455583
mojtaba <bakhtiar.mojtaba@gmail.com>
2022-02-06 13:13:58 +0100

    refactor ProductRepository

--- src/main/java/de/mo/coding/WebShop/repository/ProductRepository.java
+++ src/main/java/de/mo/coding/WebShop/repository/ProductRepository.java
@@ -2,49 +2,54 @@ package de.mo.coding.WebShop.repository;
 
 import de.mo.coding.WebShop.model.ProductResponse;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductRepository {
 
-    public List<ProductResponse> findAll(String tag) {
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "AMD Ryzen 9 5950x",
-                        "this is a product",
-                        89400,
-                        Arrays.asList("AMD", "Processor")
-                ),
-                new ProductResponse(
-                        "2",
-                        "Intel Core i9-9900KF",
-                        "this is a product",
-                        75400,
-                        Arrays.asList("Intel", "Processor")
-                ),
-                new ProductResponse(
-                        "3",
-                        "NDIVIA GeForce GTX 1000 Ti Black Edition 11GB",
-                        "this is a product",
-                        49800,
-                        Arrays.asList("NDIVIA", "graphics")
-                )
-        );
-        if (tag == null) return products;
-        tag = tag.toLowerCase();
-        List<ProductResponse> filtered = new ArrayList<>();
+    private final List<ProductResponse> products =
+            Arrays.asList(
+                    new ProductResponse(
+                            "1",
+                            "AMD Ryzen 9 5950x",
+                            "this is a product",
+                            89400,
+                            Arrays.asList("AMD", "Processor")
+                    ),
+                    new ProductResponse(
+                            "2",
+                            "Intel Core i9-9900KF",
+                            "this is a product",
+                            75400,
+                            Arrays.asList("Intel", "Processor")
+                    ),
+                    new ProductResponse(
+                            "3",
+                            "NDIVIA GeForce GTX 1000 Ti Black Edition 11GB",
+                            "this is a product",
+                            49800,
+                            Arrays.asList("NDIVIA", "graphics")
+                    )
+            );
+    ;
 
-        for (ProductResponse p : products) {
-            List<String> lowercaseTags = new ArrayList<>();
-            for (String t : p.getTags()) {
-                lowercaseTags.add(t.toLowerCase());
-            }
-            if (lowercaseTags.contains(tag)) {
-                filtered.add(p);
-            }
-        }
-        return filtered;
+    public List<ProductResponse> findAll(String tag) {
+
+        if (tag == null) return products;
+
+        String finalTag = tag.toLowerCase();
+        return products
+                .stream()
+                .filter(p -> lowerCaseTags(p).contains(finalTag))
+                .collect(Collectors.toList());
+    }
+
+
+    private List<String> lowerCaseTags(ProductResponse p) {
+        return p.getTags()
+                .stream()
+                .map(t -> t.toLowerCase())
+                .collect(Collectors.toList());
     }
 }
